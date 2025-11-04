06b81bb428eaf32692e62478151fb7abb457e60b
Franco <francoezequielmassaccesi@gmail.com>
2025-11-04 16:38:08 -0300

    exercicio cypress concluido

--- cypress.config.js
+++ cypress.config.js
@@ -6,4 +6,17 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/relatorio',
+    overwrite: false,
+    html: true,
+    json: false,
+    reportFilename: "relatorio-teste", // nome fixo do arquivo
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: true,
+    timestamp: 'ddmmyyyy'
+    //timestamp: 'ddmmyyyy_HHMMss'
+  }
 });
