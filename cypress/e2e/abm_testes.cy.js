3980649e9445b6014ead113190dbbf1d57bc73d0
Franco <francoezequielmassaccesi@gmail.com>
2025-11-06 09:50:25 -0300

    exercicio cypress concluido

--- cypress/e2e/abm_testes.cy.js
+++ cypress/e2e/abm_testes.cy.js
@@ -24,8 +24,13 @@ describe('Debe renderizar corretamente', () => {
     it('Deve editar um contato', () => {
         cy.get('.edit').first().click()
         cy.get('input[placeholder="Nome"').clear().type('Gian Souza')
-        cy.get('input[placeholder="E-mail"').clear().type('ogiansouza@ebac.com.br')
+        cy.get('input[placeholder="E-mail"').clear().type('giansouza@ebac.com.br')
         cy.get('input[placeholder="Telefone"').clear().type('11912345678')
+
+        cy.get('input[placeholder="Nome"').should('have.value', 'Gian Souza'); 
+        cy.get('input[placeholder="E-mail"').should('have.value', 'giansouza@ebac.com.br')
+        cy.get('input[placeholder="Telefone"').should('have.value', '11912345678')
+
         cy.get('.alterar').click()
         cy.get('.contato').should('have.length', 3)
         cy.get('.adicionar').should('exist');
